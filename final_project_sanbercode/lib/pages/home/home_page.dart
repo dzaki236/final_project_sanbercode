34d9a5dd9688dbcc9ffa546c1c659a966b871340
dzaki236 <dzakiahnafz@gmail.com>
2025-04-22 10:21:14 +0700

    Menambahkan fitur refresh data di halaman

--- final_project_sanbercode/lib/pages/home/home_page.dart
+++ final_project_sanbercode/lib/pages/home/home_page.dart
@@ -22,213 +22,225 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColor.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /// AppBar
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Hi there!',
-                              style:
-                                  AppFont.nunitoSansBold.copyWith(fontSize: 20),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              'What are you looking for today?',
-                              style: AppFont.nunitoSansRegular
-                                  .copyWith(fontSize: 12),
-                            ),
-                          ],
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            authController.signOut(context);
-                          },
-                          child: const Icon(
-                            Icons.logout,
-                            color: AppColor.dark,
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await Future.wait([
+              productController.getProductsList(),
+              outletController.getOutletList(),
+            ]);
+          },
+          color: AppColor.primary,
+          backgroundColor: AppColor.white,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /// AppBar
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hi there!',
+                                style: AppFont.nunitoSansBold
+                                    .copyWith(fontSize: 20),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                'What are you looking for today?',
+                                style: AppFont.nunitoSansRegular
+                                    .copyWith(fontSize: 12),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: AppColor.gray),
-                        borderRadius: BorderRadius.circular(28),
+                          GestureDetector(
+                            onTap: () {
+                              authController.signOut(context);
+                            },
+                            child: const Icon(
+                              Icons.logout,
+                              color: AppColor.dark,
+                            ),
+                          ),
+                        ],
                       ),
-                      child: const StandSearchBar(
-                          'Search cake, cookies, anything..'),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                  ],
-                ),
-
-                /// EndAppBar
-                const SizedBox(
-                  height: 16,
-                ),
-
-                /// Banner
-                SizedBox(
-                  height: 100,
-                  width: double.infinity,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.only(
-                            left: index == 0 ? 32 : 0, right: 16, top: 16),
-                        child: const BannerImage('assets/images/banner.png'),
-                      );
-                    },
-                    itemCount: 3,
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: AppColor.gray),
+                          borderRadius: BorderRadius.circular(28),
+                        ),
+                        child: const StandSearchBar(
+                            'Search cake, cookies, anything..'),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                    ],
                   ),
-                ),
 
-                /// EndBanner
+                  /// EndAppBar
+                  const SizedBox(
+                    height: 16,
+                  ),
 
-                const SizedBox(
-                  height: 24,
-                ),
-
-                /// Categories
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Our Product',
-                          style: AppFont.nunitoSansBold
-                              .copyWith(color: AppColor.dark, fontSize: 16),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Get.toNamed(ProductRoutes.products);
-                          },
-                          child: Text(
-                            'All Product',
-                            style: AppFont.nunitoSansBold.copyWith(
-                                color: AppColor.primary, fontSize: 12),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    GetBuilder(
-                      init: productController,
-                      builder: (_) {
-                        if (productController.isLoading) {
-                          return const Center(
-                            child: CircularProgressIndicator(
-                                color: AppColor.primary),
-                          );
-                        }
-                        return SizedBox(
-                            height: 260,
-                            width: double.infinity,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              physics: const AlwaysScrollableScrollPhysics(),
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(
-                                      right: 16, top: 16, bottom: 20),
-                                  child: SingleChildScrollView(
-                                    child: ProductCard(
-                                      url:
-                                          "${productController.products[index].productImage}",
-                                      productName:
-                                          "${productController.products[index].productName}-${productController.products[index].id}",
-                                      productPrice: productController
-                                          .products[index].productPrice,
-                                      onPressed: () {
-                                        Get.toNamed(
-                                          "${ProductRoutes.products}/${productController.products[index].id}",
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                );
-                              },
-                              itemCount: productController
-                                  .products.length, // diambil length nya..
-                            ));
+                  /// Banner
+                  SizedBox(
+                    height: 100,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                              left: index == 0 ? 32 : 0, right: 16, top: 16),
+                          child: const BannerImage('assets/images/banner.png'),
+                        );
                       },
+                      itemCount: 3,
                     ),
-                  ],
-                ),
+                  ),
 
-                /// Outlets
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Explore Shop',
-                      style: AppFont.nunitoSansBold
-                          .copyWith(color: AppColor.dark, fontSize: 16),
-                    ),
-                    const SizedBox(height: 12),
-                    GetBuilder(
-                        init: outletController,
+                  /// EndBanner
+
+                  const SizedBox(
+                    height: 24,
+                  ),
+
+                  /// Categories
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Our Product',
+                            style: AppFont.nunitoSansBold
+                                .copyWith(color: AppColor.dark, fontSize: 16),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Get.toNamed(ProductRoutes.products);
+                            },
+                            child: Text(
+                              'All Product',
+                              style: AppFont.nunitoSansBold.copyWith(
+                                  color: AppColor.primary, fontSize: 12),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      GetBuilder(
+                        init: productController,
                         builder: (_) {
-                          if (outletController.isLoading) {
+                          if (productController.isLoading) {
                             return const Center(
                               child: CircularProgressIndicator(
                                   color: AppColor.primary),
                             );
                           }
-                          return GridView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2, // number of items in each row
-                              mainAxisSpacing: 12.0, // spacing between rows
-                              crossAxisSpacing: 12.0, // spacing between columns
-                            ),
-                            itemCount: outletController
-                                .outlets.length, // diambil length nya..
-                            itemBuilder: (context, index) {
-                              final outlet = outletController.outlets[index];
-                              return OutletImage(
-                                url: outlet.image ??
-                                    'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=',
-                                text: "${outlet.address}",
-                              );
-                            },
-                          );
-                        }),
-                  ],
-                ),
+                          return SizedBox(
+                              height: 260,
+                              width: double.infinity,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                shrinkWrap: true,
+                                physics: const AlwaysScrollableScrollPhysics(),
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 16, top: 16, bottom: 20),
+                                    child: SingleChildScrollView(
+                                      child: ProductCard(
+                                        url:
+                                            "${productController.products[index].productImage}",
+                                        productName:
+                                            "${productController.products[index].productName}-${productController.products[index].id}",
+                                        productPrice: productController
+                                            .products[index].productPrice,
+                                        onPressed: () {
+                                          Get.toNamed(
+                                            "${ProductRoutes.products}/${productController.products[index].id}",
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  );
+                                },
+                                itemCount: productController
+                                    .products.length, // diambil length nya..
+                              ));
+                        },
+                      ),
+                    ],
+                  ),
 
-                /// EndOutlets
-                const SizedBox(
-                  height: 36,
-                ),
-              ],
+                  /// Outlets
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Explore Shop',
+                        style: AppFont.nunitoSansBold
+                            .copyWith(color: AppColor.dark, fontSize: 16),
+                      ),
+                      const SizedBox(height: 12),
+                      GetBuilder(
+                          init: outletController,
+                          builder: (_) {
+                            if (outletController.isLoading) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                    color: AppColor.primary),
+                              );
+                            }
+                            return GridView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount:
+                                    2, // number of items in each row
+                                mainAxisSpacing: 12.0, // spacing between rows
+                                crossAxisSpacing:
+                                    12.0, // spacing between columns
+                              ),
+                              itemCount: outletController
+                                  .outlets.length, // diambil length nya..
+                              itemBuilder: (context, index) {
+                                final outlet = outletController.outlets[index];
+                                return OutletImage(
+                                  url: outlet.image ??
+                                      'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=',
+                                  text: "${outlet.address}",
+                                );
+                              },
+                            );
+                          }),
+                    ],
+                  ),
+
+                  /// EndOutlets
+                  const SizedBox(
+                    height: 36,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
